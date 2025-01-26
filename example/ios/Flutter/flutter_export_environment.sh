60d9fb35a1d5d7d04bae741f33bc9150e69257c9
amsyary <amsyary.skenza313@gmail.com>
2025-01-26 14:26:49 +0800

    update wakelock

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Amsyari\Downloads\lecle_yoyo_player-0.0.8\example"
+export "FLUTTER_APPLICATION_PATH=D:\Livestreaming App\yoyo_player\lecle_yoyo_player-0.0.8\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
